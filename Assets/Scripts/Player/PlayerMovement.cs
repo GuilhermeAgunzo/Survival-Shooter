4cd171f67515306416d3e2176e9f562da5173c81
Guilherme Agunzo <g.agunzo@gmail.com>
2016-11-18 22:45:48 -0200

    Personagem, movimentação e transição de animações
    Colocando o personagem na cena(scene), criando a movimentação do mesmo e
    também sua rotação através do mouse, e definindo transição de animação
    para quando o personagem se movimentar/parar/morrer

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,5 +2,65 @@
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        // Pega as informações dos botões/analógicos de movimento horizontal e vertical
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Turning();
+        Animating(h, v);
+    }
+
+    // Função de movimentação do personagem, recebe como parametro os valores de movimento das variaveis h e v
+    void Move (float h, float v)
+    {
+        movement.Set(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        // Movimenta o personagem na direção indicada
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        // Cria um raio/laser(invisivel) na camera que aponta para a posição do mouse
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        // Testa se o raio está tocando o chão, caso verdadeiro a mira do jogador 
+        // é rotacionada para a direção do mouse
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    //Função de animação, quando o jogador se movimenta a animação é disparada
+    void Animating(float h, float v)
+    {
+        bool walking = h != 0f || v != 0f;
+        anim.SetBool("IsWalking", walking);
+    }
 }
